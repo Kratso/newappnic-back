5ae5d6afb38301d907bdca8f9cf48d3c7667ba96
Kratso <kratsothelsamar@gmail.com>
2022-10-29 20:23:54 +0200

    feat: 🎸 finished auth API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,64 @@
 require('dotenv').config();
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
 import config from 'config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dbConnection from './utils/db-connection';
+import userRouter from './routes/user.route';
+import authRouter from './routes/auth.route';
+
 
 const app = express();
+
+// Middleware
+
+// Body Parser
+app.use(express.json({ limit: '10kb' }));
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// CORS
+app.use(
+  cors({
+    origin: config.get<string>('origin'),
+    credentials: true,
+  })
+);
+
+// Routing
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+
+// Testing
+app.get('/healthChecker', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to this API 😎😎😎😎',
+  });
+});
+
+// Unknown Routes
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+// Global Error Handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 const port = config.get<number>('port');
 app.listen(port, () => {
