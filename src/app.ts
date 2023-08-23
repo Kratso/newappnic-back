2984eacc9d5ed2f0216e26511ec08bc5e161bf0c
Kratso <kratsothelsamar@gmail.com>
2023-08-23 22:13:50 +0200

    feat: 🎸 added discord incorporation

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,67 @@
-require('dotenv').config();
-import express, { NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
-import config from 'config';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dbConnection from './utils/db-connection';
-import userRouter from './routes/user.route';
-import authRouter from './routes/auth.route';
-import viajeRouter from './routes/viaje.route';
-import conceptoRouter from './routes/concepto.route';
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import config from "config";
+import cookieParser from "cookie-parser";
+import dbConnection from "./utils/db-connection";
+import userRouter from "./routes/user.route";
+import authRouter from "./routes/auth.route";
+import viajeRouter from "./routes/viaje.route";
+import conceptoRouter from "./routes/concepto.route";
+import { Client, IntentsBitField, ActivityType, TextChannel } from "discord.js";
+import { discordService } from "./services/discord.service";
 
 const app = express();
+
+// Discord Bot
+
+export const client = new Client({
+  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+});
+
+discordService.init();
 
 // Middleware
 
 // Body Parser
-app.use(express.json({ limit: '40kb' }));
+app.use(express.json({ limit: "40kb" }));
 
 // Cookie Parser
 app.use(cookieParser());
 
 // Logging
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
   }
   next();
 });
 
 // Routing
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/viaje', viajeRouter);
-app.use('/api/conceptos', conceptoRouter)
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/viaje", viajeRouter);
+app.use("/api/conceptos", conceptoRouter);
 
 // Testing
-app.get('/healthChecker', (req: Request, res: Response, next: NextFunction) => {
+app.get("/healthChecker", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
-    status: 'success',
-    message: 'Welcome to this API 😎😎😎😎',
+    status: "success",
+    message: "Welcome to this API 😎😎😎😎",
   });
 });
 
 // Unknown Routes
-app.all('*', (req: Request, res: Response, next: NextFunction) => {
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
   next(err);
@@ -60,7 +69,7 @@ app.all('*', (req: Request, res: Response, next: NextFunction) => {
 
 // Global Error Handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
   err.statusCode = err.statusCode || 500;
 
   res.status(err.statusCode).json({
@@ -69,8 +78,8 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-const port = config.get<number>('port');
+const port = config.get<number>("port");
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
-  dbConnection()
+  dbConnection();
 });
---
+++ src/services/discord.service.ts
@@ -0,0 +1,136 @@
+import { client } from "../app";
+import Viaje from "../models/viaje.model";
+import cron from "node-cron";
+import { EmbedBuilder, TextChannel } from "discord.js";
+
+export const discordService = {
+  init: async () => {
+    let channel: TextChannel;
+
+    client.on("ready", async () => {
+      await client.channels
+        .fetch(process.env.DISCORD_CHANNEL_ID || "")
+        .then((a) => {
+          channel = a as TextChannel;
+        });
+      console.log(`Logged in as ${client?.user?.tag}!`);
+    });
+
+    cron.schedule("0 0 */8 * * * ", async () => {
+      // get all viajes from db
+      const viajes = await Viaje.find({})
+        .populate("participantes")
+        .populate("conceptos")
+        .populate("contable")
+        .exec();
+
+      // check if theirs any active viaje, active being that the viaje has not ended yet
+      const activeViajes = viajes.filter(
+        (viaje) => (viaje?.end_date as Date) > new Date()
+      );
+
+      // calculate the standing balance, separated by currency, of every user in every active viaje
+      const balances = activeViajes.map((viaje) => {
+        const participantes = viaje.participantes;
+        const conceptosPorDivisa = (viaje.conceptos as any[]).reduce(
+          (acc, concepto) => {
+            const divisa = concepto.divisa;
+            acc[divisa] = acc[divisa] ? [...acc[divisa], concepto] : [concepto];
+            return acc;
+          },
+          {} as { [key: string]: any[] }
+        );
+
+        const participantesBalances = participantes.map((participante) => {
+          const balances: any[] = [];
+
+          Object.entries(conceptosPorDivisa).forEach(([divisa, conceptos]) => {
+            const conceptosPagados = (conceptos as any[]).filter(
+              (concepto) =>
+                concepto.pagador._id.toString() === participante._id.toString()
+            );
+            const conceptosParticipados = (conceptos as any[]).filter(
+              (concepto) =>
+                (concepto.participantes as any[]).some(
+                  (participanteConcepto) =>
+                    participanteConcepto.usuario._id.toString() ===
+                    participante._id.toString()
+                )
+            );
+
+            const totalPagado = conceptosPagados.reduce(
+              (acc, concepto) =>
+                acc +
+                (concepto.precio *
+                  concepto.unidades *
+                  concepto.participantes.filter((user: any) => !user.pagado)
+                    .length) /
+                  concepto.participantes.length,
+              0
+            );
+
+            const totalParticipado = conceptosParticipados.reduce(
+              (acc, concepto) => {
+                const total =
+                  acc +
+                  (concepto.participantes.find(
+                    (user: any) =>
+                      user.usuario._id.toString() ===
+                      participante._id.toString()
+                  ).pagado
+                    ? 0
+                    : (concepto.precio * concepto.unidades) /
+                      concepto.participantes.length);
+                return total;
+              },
+              0
+            );
+
+            const balance = totalParticipado - totalPagado;
+            balances.push({ divisa, balance });
+          });
+          return { name: participante.name, balances };
+        });
+
+        return { viaje: viaje.destino, participantesBalances };
+      });
+
+
+      let fieldsDeudas = [];
+
+      balances.forEach((viaje) => {
+        fieldsDeudas = viaje.participantesBalances.map((participante) => {
+          return {
+            name: participante.name,
+            value: participante.balances.reduce((acc, balance) => {
+              acc += `${balance.divisa}: ${balance.balance}\n`;
+              return acc;
+            }, ""),
+            inline: true,
+          };
+        });
+        const viajeEmbed = new EmbedBuilder();
+
+        viajeEmbed
+          .setTitle(`Deudas de ${viaje.viaje}`)
+          .setURL("http://travel.pibardos.club/stats")
+          .setDescription(
+            "Para más información, visita la web http://travel.pibardos.club"
+          )
+          .setThumbnail("https://i.imgur.com/lk2wCDS.png")
+          .addFields(...fieldsDeudas)
+          .setImage("https://i.imgur.com/lyNs02i.png")
+          .setTimestamp()
+          .setFooter({
+            text: "¿Es esto un error? Contacta con el soporte técnico en soporte@pibardos.club",
+            iconURL: "https://i.imgur.com/lk2wCDS.png",
+          });
+
+          channel.send({ embeds: [viajeEmbed] });
+      });
+
+    });
+
+    client.login(process.env.DISCORD_TOKEN);
+  },
+};
